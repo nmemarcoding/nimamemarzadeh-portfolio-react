b4791b914a703e37bb7da8bc41770f288297aef4
nmemarcoding <nmemarcoding@gmail.com>
2022-04-06 11:16:59 -0700

    Portfolio Created

---
+++ portfolio-react/src/components/portfolioData.js
@@ -0,0 +1,36 @@
+export const projects = [{
+    name: "password generator",
+    img: "images/passwordgeneratoe.png",
+    git: "https://github.com/nmemarcoding/passwordGenerator",
+    web: "https://nmemarcoding.github.io/passwordGenerator/"
+}, {
+    name: "Work Day Scheduler",
+    img: "images/workDay.png",
+    git: "https://github.com/nmemarcoding/passwordGenerator",
+    web: "https://nmemarcoding.github.io/passwordGenerator/"
+}, {
+    name: "WeaderDashbpard",
+    img: "images/weather.png",
+    git: "https://github.com/nmemarcoding/WeaderDashbpard",
+    web: "https://nmemarcoding.github.io/WeaderDashbpard/"
+}, {
+    name: "Note Taker",
+    img: "images/noteTaker.png",
+    git: "https://github.com/nmemarcoding/Note-Taker-1400",
+    web: "https://note-taker-1400.herokuapp.com/"
+}, {
+    name: "Tech-Blog",
+    img: "images/tech-blog.png",
+    git: "https://github.com/nmemarcoding/Tech-Blog-1400",
+    web: "https://tech-blog-1400.herokuapp.com/"
+}, {
+    name: "What's For Dinner",
+    img: "images/dinner.png",
+    git: "https://github.com/stahljohnmark/whatsfordinner",
+    web: "https://stahljohnmark.github.io/whatsfordinner/"
+}, {
+    name: "4-the-record",
+    img: "images/record.png",
+    git: "https://github.com/dmevola/4-the-record",
+    web: "https://record-1400.herokuapp.com/"
+}]
