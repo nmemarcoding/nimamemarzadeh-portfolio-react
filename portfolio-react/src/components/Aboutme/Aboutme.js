35a52291a831bcf83c2a82e4e5b5b4131ea9be75
nmemarcoding <nmemarcoding@gmail.com>
2022-05-12 20:37:55 -0700

    resime changed

--- portfolio-react/src/components/Aboutme/Aboutme.js
+++ portfolio-react/src/components/Aboutme/Aboutme.js
@@ -6,5 +6,6 @@ export default function Aboutme() {
         <div className="Aboutme">
             <h3 className="aboueme-summery">Full stack web Developer with ability to build applications from the ground up. Skilled at writing well-designed, testable, and efficient code using current best practices in Web development. Fast learner, hard worker, and team player who is proficient in an array of scripting languages and web tools. Background in management, sales, and customer service with current objective of transitioning into a full-time junior software engineer or web development position.</h3>
         </div>
+        
     )
 }
